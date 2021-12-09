ee99e13636f3144d43ccba9d3f2bc57129054ee4
Marick <marickmanrho@gmail.com>
2021-12-09 17:39:40 +0100

    Link bashrc

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,9 @@ BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # Install Zsh
 sudo apt-get install zsh
 
+# bash
+ln -s ${BASEDIR}/bashrc ~/.bashrc
+
 # vim
 ln -s ${BASEDIR}/vimrc ~/.vimrc
 ln -s ${BASEDIR}/vim ~/.vim
@@ -18,7 +21,5 @@ ln -s ${BASEDIR}/zshrc ~/.zshrc
 
 # Pure zsh theme
 mkdir -p "${BASEDIR}/zsh"
-
-git clone https://github.com/sindresorhus/pure.git "${BASEDIR}/zsh/pure"
 
 ln -s ${BASEDIR}/zsh ~/.zsh
